9dbb967dd077242acb5deb56bd187cd2be588947
zosia <zofiaszczech1@gmail.com>
2017-03-27 18:41:59 +0200

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,21 +21,20 @@ module.exports = function(grunt) {
             dest: 'images/build/'
           }]
       }
+    },
+    watch: {
+      scripts: {
+          files: ['*.sass'],
+          tasks: ['sass'],
+          options: {
+              spawn: false,
+          },
+      } 
     }
-  
-  });
-  watch: {
-    scripts: {
-        files: ['sass/*.sass'],
-        tasks: ['sass'],
-        options: {
-            spawn: false,
-        },
-    } 
-  }
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
   grunt.registerTask('default', ['sass','imagemin']);
 };
